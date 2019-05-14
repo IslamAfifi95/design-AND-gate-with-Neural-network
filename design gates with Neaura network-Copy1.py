8445333fef88652b86dec54d57ae3e91e69063c7
IslamAfifi95 <50583062+IslamAfifi95@users.noreply.github.com>
2019-05-14 02:58:29 +0200

    Update design gates with Neaura network-Copy1.py

--- design gates with Neaura network-Copy1.py
+++ design gates with Neaura network-Copy1.py
@@ -1,18 +1,11 @@
 
 # coding: utf-8
 
-# In[ ]:
-
-
 # Binary function
 def BF(x):
     if x>0:
         return 1
     return 0
-
-
-# In[3]:
-
 
 # output function
 def OF(x):
@@ -20,16 +13,12 @@ def OF(x):
         return True
     return False
 
-
-# In[15]:
-
-
-
 w = np.array([-2, 1,2])
 N=0.001
 Input=[(1,1,1),(1,0,1),(0,1,1),(0,0,1)]
 test_input = np.array([[1, 1,1], [1, 0,1],[0,1,1],[0,0,1]])
 correct_output=[True,False,False,False]
+# trian my preceptron
 for i in range(40):
     index=0
     count=1    
@@ -45,41 +34,15 @@ for i in range(40):
     if count==1:
         break
     
-    
-
-    
-        
-        
-
-
-# In[18]:
-
-
-test=w[0]*1+w[1]*1+w[2]*1
-
-if test>=0:
-        print(True)
-else:
-     print(False)
-
-
-# In[30]:
-
-
+    #using the programming here
 z=[int(input("Enter first input")),int(input("Enter second input")),1]
-z
 def AND(z):
     print(OF(BF(np.sum(z*w))))
     
-
-
-# In[31]:
-
-
 AND(z)
 
 
-# In[ ]:
+
 
 
 
